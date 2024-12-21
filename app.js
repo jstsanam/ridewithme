5ea0b795f500e8defefcea21e28fd1b332cb7f9b
Sanam Yadav <jstsanam@gmail.com>
2024-12-21 17:32:57 +0530

    added start script in package.json

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
-
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
